2cffff9929b7ca61c43550131ab8adac75eca517
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-06-30 14:35:50 +0300

    Update coursera.py
    function of functions is corrected

--- coursera.py
+++ coursera.py
@@ -8,33 +8,47 @@ from lxml import html
 from bs4 import BeautifulSoup
 
 
-def get_courses_list():
+def get_data_from_xml():
     response = requests.get('https://www.coursera.org/sitemap~www~courses.xml')
     content = response.text.encode('utf-8')
-    parsed_document = html.fromstring(content)
-    courses_list = parsed_document.xpath('//loc/text()')
-    return courses_list
+    return content
 
 
-def get_course_info(course_slug):
-    response = requests.get(course_slug)
+def get_courses_urls_list(content):
+    parsed_urls_document = html.fromstring(content)
+    courses_urls_list = parsed_urls_document.xpath('//loc/text()')
+    return courses_urls_list
+
+
+def get_parsed_html(course_url):
+    response = requests.get(course_url)
     content = response.content.decode('utf-8')
-    soup = BeautifulSoup(content, 'html.parser')
-    title = soup.find('h1', {'class': 'title display-3-text'}).text
-    lang = soup.find('div', {'class': 'rc-Language'}).text
-    startdate = soup.find('div', {
+    course_page = BeautifulSoup(content, 'html.parser')
+    return course_page
+
+
+def get_course_info(page):
+    title = page.find('h1', {'class': 'title display-3-text'}).text
+    lang = page.find('div', {'class': 'rc-Language'}).text
+    startdate = page.find('div', {
         'class': 'startdate rc-StartDateString caption-text'
     }).text
-    weeks = len(soup.findAll('div', {'class': 'week'}))
-    rating_tags = soup.findAll('div', {'class': 'ratings-text headline-2-text'})
+    weeks = len(page.findAll('div', {'class': 'week'}))
+    rating_tags = page.findAll('div', {'class': 'ratings-text headline-2-text'})
     rating = ''
     for rating_tag in rating_tags:
         rating += rating_tag.text
-    course_info = [title, lang, startdate, weeks, rating]
+    course_info = {
+        'title': title,
+        'language': lang,
+        'startdate': startdate,
+        'weeks': weeks,
+        'rating': rating
+    }
     return course_info
 
 
-def output_courses_info_to_xlsx(filepath, courses_info_list):
+def output_courses_info_to_xlsx(courses_info_list):
     workbook = openpyxl.Workbook()
     worksheet = workbook.active
     title_list = ['TITLE', 'LANGUAGE', 'DATE TO START', 'WEEKS', 'RATING']
@@ -47,11 +61,22 @@ def output_courses_info_to_xlsx(filepath, courses_info_list):
         worksheet['E1']
     ]:
         title.font = Font(bold=True)
-    for course_info_list in courses_info_list:
-        worksheet.append(course_info_list)
+    for course_info_dic in courses_info_list:
+        course_info = [
+            course_info_dic['title'],
+            course_info_dic['language'],
+            course_info_dic['startdate'],
+            course_info_dic['weeks'],
+            course_info_dic['rating']
+        ]
+        worksheet.append(course_info)
     for column_cells in worksheet.columns:
         length = max(len(str(cell.value)) for cell in column_cells)
         worksheet.column_dimensions[column_cells[0].column].width = length
+    return workbook
+
+
+def save_data_to_xlsx(workbook, filepath):
     workbook.save(filepath)
 
 
@@ -61,15 +86,18 @@ if __name__ == '__main__':
     dir_path = sys.argv[1]
     file_name = 'coursera_courses.xlsx'
     file_path = os.path.join(dir_path, file_name)
-    courses = get_courses_list()
-    max_length = 20
-    min_length = 0
+    courses_data_from_xml = get_data_from_xml()
+    courses_urls_list = get_courses_urls_list(courses_data_from_xml)
+    number_of_courses = 20
     courses_info_list = []
     try:
-        while len(courses_info_list) < max_length:
-            index = random.randint(min_length, len(courses))
-            courses_info_list.append(get_course_info(courses[index]))
+        short_urls_list = random.sample(courses_urls_list, number_of_courses)
+        for course_url in short_urls_list:
+            course_page = get_parsed_html(course_url)
+            course_info = get_course_info(course_page)
+            courses_info_list.append(course_info)
     except(IndexError, AttributeError):
         pass
-    output_courses_info_to_xlsx(file_path, courses_info_list)
+    excel_workbook = output_courses_info_to_xlsx(courses_info_list)
+    save_data_to_xlsx(excel_workbook, file_path)
     print('File "coursera_courses.xlsx" was successfully created')
