04f88516e61593c379feb4444bb9e6f46e6bad65
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-07-01 15:20:37 +0300

    Update coursera.py
    functions corrected

--- coursera.py
+++ coursera.py
@@ -14,24 +14,22 @@ def get_data_from_url(url):
     return content
 
 
-def get_courses_urls_list():
-    xml_feed = 'https://www.coursera.org/sitemap~www~courses.xml'
-    xml_data = get_data_from_url(xml_feed).encode('utf-8')
-    parsed_urls_document = html.fromstring(xml_data)
+def get_courses_urls_list(xml_data_courses):
+    parsed_urls_document = html.fromstring(xml_data_courses)
     courses_list = parsed_urls_document.xpath('//loc/text()')
     return courses_list
 
 
-def get_course_info(coursera_course_url):
-    course_data = get_data_from_url(coursera_course_url)
-    soup = BeautifulSoup(course_data, 'html.parser')
-    title = soup.find('h1', {'class': 'title display-3-text'}).text
-    lang = soup.find('div', {'class': 'rc-Language'}).text
-    startdate = soup.find('div', {
+def get_course_info(html_data):
+    title = html_data.find('h1', {'class': 'title display-3-text'}).text
+    lang = html_data.find('div', {'class': 'rc-Language'}).text
+    startdate = html_data.find('div', {
         'class': 'startdate rc-StartDateString caption-text'
     }).text
-    weeks = len(soup.findAll('div', {'class': 'week'}))
-    rating_tags = soup.findAll('div', {'class': 'ratings-text headline-2-text'})
+    weeks = len(html_data.findAll('div', {'class': 'week'}))
+    rating_tags = html_data.findAll('div', {
+        'class': 'ratings-text headline-2-text'
+    })
     rating = ''
     for rating_tag in rating_tags:
         rating += rating_tag.text
@@ -83,16 +81,23 @@ if __name__ == '__main__':
     dir_path = sys.argv[1]
     file_name = 'coursera_courses.xlsx'
     file_path = os.path.join(dir_path, file_name)
-    courses_urls_list = get_courses_urls_list()
+
+    xml_feed = 'https://www.coursera.org/sitemap~www~courses.xml'
+    xml_data = get_data_from_url(xml_feed).encode('utf-8')
+    courses_urls_list = get_courses_urls_list(xml_data)
+
     number_of_courses = 20
     courses_info_list = []
     try:
         short_urls_list = random.sample(courses_urls_list, number_of_courses)
         for course_url in short_urls_list:
-            course_info = get_course_info(course_url)
+            course_data = get_data_from_url(course_url)
+            course_data_html = BeautifulSoup(course_data, 'html.parser')
+            course_info = get_course_info(course_data_html)
             courses_info_list.append(course_info)
     except(IndexError, AttributeError):
         pass
+
     excel_workbook = output_courses_info_to_xlsx(courses_info_list)
     save_data_to_xlsx(excel_workbook, file_path)
     print('File "coursera_courses.xlsx" was successfully created')
